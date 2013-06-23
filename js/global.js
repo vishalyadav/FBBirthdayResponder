35f71aca7721179c2804ce3b0937d3b5456b0243
vishal <vishal_s_yadav@yahoo.com>
2013-06-22 18:21:45 -0700

    2nd commit

---
+++ js/global.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	// Additional JS functions here
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '486095544793129', // App ID
+        channelUrl : '/channel.html', // Channel File
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true  // parse XFBML
+      });
+
+    // Additional init code here
+    };
+	$("#login").click(function(){
+		FB.login(function(response) {
+			if(response.authResponse) {
+				testAPI();
+			}
+			else {
+				//login was cancelled
+			}
+		});
+	});
+  // Load the SDK asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "http://connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+});
